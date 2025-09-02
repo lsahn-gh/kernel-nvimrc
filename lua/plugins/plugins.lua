cc00be1dcb7b5a4eeda0866167d3e6d1b4733458
Leesoo Ahn <lsahn@ooseel.net>
2025-09-02 14:06:54 +0900

    ???

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -84,7 +84,7 @@ require("lazy").setup({
         config = function() require("fzf-lua").setup({}) end,
     },
 
-    -- Rustaceanvim
+    -- Rust LSP
     { "mrcjkb/rustaceanvim",
         ft = "rust",
         init = function()
@@ -102,4 +102,68 @@ require("lazy").setup({
             }
         end,
     },
+
+    -- CPP LSP
+    { "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "cpp" }
+        },
+    },
+    { "p00f/clangd_extensions.nvim",
+        lazy = true,
+        config = function() end,
+        opts = {
+            inlay_hints = {
+                inline = false,
+            },
+        },
+    },
+    { "neovim/nvim-lspconfig",
+        config = function() end,
+        opts = {
+            servers = {
+                -- Ensure mason installs the server
+                clangd = {
+                    keys = {
+                        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+                    },
+                    root_dir = function(fname)
+                        return require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname)
+                    end,
+                    capabilities = {
+                        offsetEncoding = { "utf-16" },
+                    },
+                    cmd = {
+                        "clangd",
+                        "--background-index",
+                        "--clang-tidy",
+                        "--header-insertion=iwyu",
+                        "--completion-style=detailed",
+                        "--function-arg-placeholders",
+                        "--fallback-style=llvm",
+                    },
+                    init_options = {
+                        usePlaceholders = true,
+                        completeUnimported = true,
+                        clangdFileStatus = true,
+                    },
+                },
+            },
+            setup = {
+                clangd = function(_, opts)
+                    local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+                    require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+                    return false
+                end,
+            },
+        },
+    },
+    { "hrsh7th/nvim-cmp",
+        optional = true,
+        opts = function(_, opts)
+            opts.sorting = opts.sorting or {}
+            opts.sorting.comparators = opts.sorting.comparators or {}
+            table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+        end,
+    }
 })
